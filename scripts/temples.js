9927fb7d4e0dd140dc458d6e823732ed78c15b7e
sdurrans <sdurrant@byupathway.edu>
2025-03-22 13:33:20 -0400

    temple final

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,6 +1,13 @@
 const currentYear = new Date().getFullYear();
 document.getElementById('currentYear').textContent = currentYear;
 
-
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
