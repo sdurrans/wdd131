70e0a73173af980b6335ce9d4cf94b15a97660cf
sdurrans <sdurrant@byupathway.edu>
2025-03-22 11:47:28 -0400

    temple album

---
+++ scripts/temples.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentYear').textContent = currentYear;
+
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
