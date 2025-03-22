fbd219b3354d2f7ed4c21f2c373a1a574937fb05
sdurrans <sdurrant@byupathway.edu>
2025-03-21 22:34:09 -0400

    bom updates

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,9 +1,33 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const list = document.querySelector('list');
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = 'X';
-li.append(deleteButton);
-list.append(li);
+
+const input = document.querySelector('#favchap'); 
+const button = document.querySelector('button'); 
+const list = document.querySelector('#list'); 
+
+button.addEventListener('click', function () {
+    const chapter = input.value.trim(); 
+
+    if (chapter !== '') {
+        const li = document.createElement('li');
+        li.textContent = chapter;
+
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.classList.add('delete');
+
+
+        li.append(deleteButton);
+
+
+        list.append(li);
+
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+        });
+
+
+        input.value = '';
+        input.focus();
+    } 
+});
